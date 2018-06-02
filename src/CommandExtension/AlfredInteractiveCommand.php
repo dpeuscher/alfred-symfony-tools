837ac97aaaf2b4d51c04d37d1f3e99850d46d81c
Dominik Peuscher <dominik.peuscher@check24.de>
2018-06-02 16:46:07 +0200

    Init and Add AlfredInteractiveCommand

---
+++ src/CommandExtension/AlfredInteractiveCommand.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace Dpeuscher\AlfredSymfonyTools\CommandExtension;
+
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+use Psr\Log\LogLevel;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+
+/**
+ * @category  alfred-symfony-tools
+ * @copyright Copyright (c) 2018 Dominik Peuscher
+ */
+class AlfredInteractiveCommand extends ContainerAwareCommand implements LoggerAwareInterface
+{
+    use LoggerAwareTrait;
+
+    /**
+     * @var string[]
+     */
+    private $acFields = [];
+
+    /**
+     * @var string[][]
+     */
+    private $acFieldsList = [];
+
+    /**
+     * @var bool
+     */
+    private $updatedBestMatches = false;
+
+    public function addArgument($name, $mode = null, $description = '', $default = null, array $allowedValues = null)
+    {
+        if (isset($allowedValues)) {
+            $this->acFields[] = $name;
+            $this->acFieldsList[$name] = $allowedValues;
+        }
+        return parent::addArgument($name, $mode, $description, $default);
+    }
+
+    public function getArgumentIdentifier(InputInterface $input, string $name)
+    {
+        if (!in_array($name, $this->acFields)) {
+            return $input->getArgument($name);
+        }
+        if (empty($this->acFieldsList[$name])) {
+            $this->log(LogLevel::NOTICE, 'There are no possible values for argument ' . $name . ' configured');
+            return null;
+        }
+        $matches = $this->getArgumentMatches($input, $name);
+        if (count($matches) == 1) {
+            return key($matches);
+        }
+
+        return null;
+    }
+
+    public function updateBestMatchKeys()
+    {
+        if ($this->updatedBestMatches) {
+            return;
+        }
+        foreach ($this->acFieldsList as $argumentName => $fields) {
+            $bestMatchKeyList = [];
+            foreach ($fields as $key => $value) {
+                if (!is_int($key)) {
+                    continue 2;
+                }
+                $words = explode(' ', $value);
+                $keyCandidate = $key;
+
+                for ($i = 0; $i < count($words); $i++) {
+                    $keyCandidate = implode(array_slice($words, 0, $i + 1));
+                    foreach ($fields as $searchString) {
+                        if ($searchString === $value) {
+                            continue;
+                        }
+                        if (stristr(str_replace(' ', '', $searchString),$keyCandidate) !== false) {
+                            continue 2;
+                        }
+                    }
+                    break;
+                }
+                $bestMatchKeyList[$keyCandidate] = $value;
+            }
+            $this->acFieldsList[$argumentName] = $bestMatchKeyList;
+        }
+        $this->updatedBestMatches = true;
+    }
+
+    public function getArgumentMatches(InputInterface $input, string $name)
+    {
+        $this->updateBestMatchKeys();
+        if (!in_array($name, $this->acFields)) {
+            return [];
+        }
+        if (empty($this->acFieldsList[$name])) {
+            $this->log(LogLevel::NOTICE, 'There are no possible values for argument ' . $name . ' configured');
+            return [];
+        }
+        $argument = $input->getArgument($name);
+        if (!isset($argument)) {
+            return [];
+        }
+        $matches = [];
+        foreach ($this->acFieldsList[$name] as $key => $value) {
+            if (stristr(str_replace(' ', '', $value), $argument) !== false) {
+                $matches[$key] = $value;
+            }
+        }
+        return $matches;
+    }
+
+    public function getSelectedArgument(InputInterface $input, string $name)
+    {
+        $key = $this->getArgumentIdentifier($input, $name);
+        if (isset($key)) {
+            return $this->acFieldsList[$name][$key];
+        }
+        return null;
+    }
+
+    /**
+     * @param string $level
+     * @param string $message
+     * @param array $context
+     */
+    protected function log(string $level, string $message, array $context = [])
+    {
+        if (isset($this->logger)) {
+            $this->logger->log($level, $message, $context);
+        } else {
+            //@codeCoverageIgnoreStart
+            trigger_error($message, [
+                LogLevel::EMERGENCY => E_USER_ERROR,
+                LogLevel::ALERT     => E_USER_ERROR,
+                LogLevel::CRITICAL  => E_USER_ERROR,
+                LogLevel::ERROR     => E_USER_ERROR,
+                LogLevel::WARNING   => E_USER_WARNING,
+                LogLevel::NOTICE    => E_USER_NOTICE,
+                LogLevel::INFO      => E_USER_NOTICE,
+                LogLevel::DEBUG     => E_USER_NOTICE,
+            ][$level]);
+            //@codeCoverageIgnoreEnd
+        }
+    }
+}
---
+++ tests/CommandExtension/AlfredInteractiveCommandTest.php
@@ -0,0 +1,217 @@
+<?php
+
+namespace Dpeuscher\AlfredSymfonyTools\Tests\CommandExtension;
+
+use Dpeuscher\AlfredSymfonyTools\CommandExtension\AlfredInteractiveCommand;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputDefinition;
+
+/**
+ * @covers \Dpeuscher\AlfredSymfonyTools\CommandExtension\AlfredInteractiveCommand
+ */
+class AlfredInteractiveCommandTest extends TestCase
+{
+    /**
+     * @var AlfredInteractiveCommand
+     */
+    protected $command;
+
+    /**
+     * @var \Closure
+     */
+    protected $getPrivateFields;
+
+    public function setup()
+    {
+        $this->command = new AlfredInteractiveCommand();
+        $this->getPrivateFields = function ($field) {
+            return $this->$field;
+        };
+    }
+
+    public function testGetSelectedArgument()
+    {
+        $input = $this->setupArguments(['test' => 'abcd']);
+
+        $this->assertEquals('abc def', $this->command->getSelectedArgument($input, 'test'));
+    }
+
+    public function testGetSelectedArgumentOfNotAcArgumentReturnsNull()
+    {
+        $input = $this->setupArguments(['test' => 'abcd']);
+
+        $this->assertEquals(null, $this->command->getSelectedArgument($input, 'test4'));
+    }
+
+    public function testGetSelectedArgumentOfEmptyWhitelistReturnsNull()
+    {
+        $input = $this->setupArguments(['test' => 'abcd']);
+
+        $this->command->setLogger(new NullLogger());
+
+        $this->assertEquals(null, $this->command->getSelectedArgument($input, 'test5'));
+    }
+
+    public function testGetSelectedArgumentOfEmptyWhitelistLogs()
+    {
+        $input = $this->setupArguments(['test' => 'abcd']);
+
+        /** @var LoggerInterface|MockObject $logger */
+        $logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
+        $logger->expects($this->once())->method('log');
+        $this->command->setLogger($logger);
+
+        $this->assertEquals(null, $this->command->getSelectedArgument($input, 'test5'));
+    }
+
+    public function testUpdateBestMatchKeys()
+    {
+        $this->setupArguments([]);
+
+        $this->command->updateBestMatchKeys();
+
+        $expected = [
+            'test'  => [
+                'abc' => 'abc def',
+                'ghi' => 'ghi',
+                'jkl' => 'jkl mno pqr',
+            ],
+            'test2' => [
+                'ABC' => 'abc def',
+                'DEF' => 'ghi',
+                'GHI' => 'jkl mno pqr',
+            ],
+            'test3' => [
+                'abcdef1'   => 'abc def1',
+                'abcghi'    => 'abc ghi',
+                'abcdefghi' => 'abc def ghi',
+                'jklmno'    => 'jkl mno pqr',
+                'jklabc'    => 'jkl abc',
+            ],
+            'test5' => [],
+        ];
+
+        $this->assertEquals($expected, $this->getPrivateFields->call($this->command, 'acFieldsList'));
+    }
+
+    public function testUpdateBestMatchKeysCanRunTwice()
+    {
+        $this->setupArguments([]);
+
+        $this->command->updateBestMatchKeys();
+        $this->command->updateBestMatchKeys();
+
+        $expected = [
+            'test'  => [
+                'abc' => 'abc def',
+                'ghi' => 'ghi',
+                'jkl' => 'jkl mno pqr',
+            ],
+            'test2' => [
+                'ABC' => 'abc def',
+                'DEF' => 'ghi',
+                'GHI' => 'jkl mno pqr',
+            ],
+            'test3' => [
+                'abcdef1'   => 'abc def1',
+                'abcghi'    => 'abc ghi',
+                'abcdefghi' => 'abc def ghi',
+                'jklmno'    => 'jkl mno pqr',
+                'jklabc'    => 'jkl abc',
+            ],
+            'test5' => [],
+        ];
+
+        $this->assertEquals($expected, $this->getPrivateFields->call($this->command, 'acFieldsList'));
+    }
+
+    public function testGetArgumentMatches()
+    {
+        $input = $this->setupArguments(['test3' => 'abc']);
+
+        $this->assertEquals([
+            'abcdef1'   => 'abc def1',
+            'abcghi'    => 'abc ghi',
+            'abcdefghi' => 'abc def ghi',
+            'jklabc'    => 'jkl abc',
+        ], $this->command->getArgumentMatches($input, 'test3'));
+    }
+
+    public function testGetArgumentMatchesOfNotAcArgumentReturnsNull()
+    {
+        $input = $this->setupArguments(['test' => 'abcd']);
+
+        $this->assertEquals([], $this->command->getArgumentMatches($input, 'test4'));
+    }
+
+    public function testGetArgumentMatchesOfEmptyWhitelistReturnsNull()
+    {
+        $input = $this->setupArguments(['test' => 'abcd']);
+
+        $this->command->setLogger(new NullLogger());
+
+        $this->assertEquals([], $this->command->getArgumentMatches($input, 'test5'));
+    }
+
+    public function testGetArgumentIdentifier()
+    {
+        $input = $this->setupArguments(['test' => 'pqr']);
+
+        $this->assertEquals('jkl', $this->command->getArgumentIdentifier($input, 'test'));
+    }
+
+    public function testGetArgumentIdentifierWithMoreMatchesReturnsNull()
+    {
+        $input = $this->setupArguments(['test3' => 'abc']);
+
+        $this->assertEquals(null, $this->command->getArgumentIdentifier($input, 'test'));
+    }
+
+    public function testAddArgument()
+    {
+        $this->command->addArgument('test', InputArgument::OPTIONAL, '', null, [
+            'abc def',
+            'ghi',
+            'jkl mno pqr',
+        ]);
+
+        $this->assertNotEmpty($this->getPrivateFields->call($this->command, 'acFieldsList'),
+            'acFieldsList was not updated');
+    }
+
+    private function setupArguments($values): ArrayInput
+    {
+        $this->command->addArgument('test', InputArgument::OPTIONAL, '', null, [
+            'abc def',
+            'ghi',
+            'jkl mno pqr',
+        ]);
+        $this->command->addArgument('test2', InputArgument::OPTIONAL, '', null, [
+            'ABC' => 'abc def',
+            'DEF' => 'ghi',
+            'GHI' => 'jkl mno pqr',
+        ]);
+        $this->command->addArgument('test3', InputArgument::OPTIONAL, '', null, [
+            'abc def1',
+            'abc ghi',
+            'abc def ghi',
+            'jkl mno pqr',
+            'jkl abc',
+        ]);
+        $this->command->addArgument('test4', InputArgument::OPTIONAL, '', null);
+        $this->command->addArgument('test5', InputArgument::OPTIONAL, '', null, []);
+        $input = new ArrayInput($values, new InputDefinition([
+            new InputArgument('test'),
+            new InputArgument('test2'),
+            new InputArgument('test3'),
+            new InputArgument('test4'),
+            new InputArgument('test5'),
+        ]));
+        return $input;
+    }
+}
